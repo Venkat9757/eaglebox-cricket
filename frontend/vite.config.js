53695637bfd3ecfe8109a19f6e2e9fb2e03feb4c
Venkat <168633261+Venkat9757@users.noreply.github.com>
2026-06-27 12:00:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://eaglebox-cricket.onrender.com',
         changeOrigin: true,
         secure: false,
       },
